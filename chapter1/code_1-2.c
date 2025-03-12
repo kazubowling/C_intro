342c978e889bdffb78e57830a22e541d17fe78ee
kazubowling <kazubowling300@gmail.com>
2025-03-12 18:02:31 +0900

    第1章サンプルコード 1-2

---
+++ chapter1/code_1-2.c
@@ -0,0 +1,12 @@
+/*  サンプルプログラム Main
+    作成者：Kazuki 作成日：2025年3月12日 */
+#include    <stdio.h>
+
+// mainブロック
+int main(void)
+{
+    int age;    // 年齢を入れる箱
+    age = 27;
+    printf("私は%d歳です\n", age);
+    return 0;
+}
