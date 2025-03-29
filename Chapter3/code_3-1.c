0506e05d6812092c3c6fcbff25b9aaf3f1343731
kazubowling <kazubowling300@gmail.com>
2025-03-29 15:58:57 +0900

    3.1.1 計算の文

---
+++ Chapter3/code_3-1.c
@@ -0,0 +1,12 @@
+#include    <stdio.h>
+
+int main(void)
+{
+    int a;  /* 変数宣言 */
+    int b;  /* 変数宣言 */
+    a = 20; /* 計算の文(代入) */
+    b = a + 5;  /* 計算の文(足し算して代入) */
+    printf("%d\n", a);
+    printf("%d\n", b);
+    return 0;
+}
